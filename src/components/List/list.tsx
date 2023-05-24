7b2b2877f9ddaeb2cc6ed553160678a4264377d6
RSitraka <Sitrakahers@gmail.com>
2023-05-24 19:58:06 +0200

    Update List
    Completion de Dialog dans la liste

--- src/components/List/list.tsx
+++ src/components/List/list.tsx
@@ -34,6 +34,49 @@ const columns: GridColDef[] = [
     editable: true,
   },
   {
+    field: 'pere',
+    headerName: 'Nom du père',
+    width: 200,
+    editable: true,
+  },
+  {
+    field: 'mere',
+    headerName: 'Nom de la mère',
+    width: 200,
+    editable: true,
+  },
+  {
+    field: 'adresse',
+    headerName: 'Adresse',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'Ddn',
+    headerName: 'Date de naissance',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'Sexe',
+    headerName: 'Sexe',
+    width: 120,
+    editable: true,
+  },
+  {
+    field: 'Phone',
+    headerName: 'Numéro Téléphone',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'cin',
+    headerName: 'Numéro CIN',
+    type: 'number',
+    width: 150,
+    editable: true,
+  },
+  {
     field: 'age',
     headerName: 'Age',
     type: 'number',
@@ -58,7 +101,14 @@ const columns: GridColDef[] = [
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 , Fokotany: 'Ambohipo'},
   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 , Fokotany: 'Ambohipo'},
   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 , Fokotany: 'Zmbohipo'},
@@ -67,6 +117,28 @@ const rows = [
   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 , Fokotany: 'Fmbohipo'},
   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 , Fokotany: 'Ambohipo'},
   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
 ];
 
 export default function list() {
@@ -86,16 +158,19 @@ export default function list() {
 
   return (
     <div>
-    <Box sx={{ height: 400, width: '100%' }}>
+    <Box sx={{ height: 470, width: '120%' }}>
       <DataGrid
         rows={rows}
         columns={columns}
         initialState={{
           pagination: {
             paginationModel: {
-              pageSize: 5,
+              pageSize: 20,
             },
           },
+        }}
+        sx={{
+          cursor: "alias",
         }}
         pageSizeOptions={[5]}
         disableRowSelectionOnClick
@@ -124,8 +199,8 @@ export default function list() {
             <CardActionArea>
               <CardMedia
                 component="img"
-                width="200"
-                height="140"
+                width="500"
+                height="200"
                 image="/public/cin.jpg"
                 alt="green iguana"
               />
@@ -141,27 +216,49 @@ export default function list() {
               sx={{
                 display: "flex",
                 flexDirection: "column",
-                marginLeft: "100px",
+                marginLeft: "50px",
                 marginTop: "5px",
                 justifyContent: "space-between"
               }}>
                 <TextField id="standard-basic" label="Nom" value={currentRow?.row?.firstName} disabled />
                 <TextField id="standard-basic" label="Prénom" value={currentRow?.row?.lastName} disabled />
                 <TextField id="standard-basic" label="Age" value={currentRow?.row?.age} disabled />       
+                <TextField id="standard-basic" label="Sexe" value={currentRow?.row?.Sexe} disabled />       
             </Box>
           </Box>
-          <Box
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "space-between",
-                marginTop: "10px"
-              }}>
-                <TextField id="standard-basic" label="Nom" value={currentRow?.row?.firstName} disabled />
-                <TextField id="standard-basic" label="Prénom" value={currentRow?.row?.lastName} disabled style={{marginTop: "10px"}}/>
-                <TextField id="standard-basic" label="Age" value={currentRow?.row?.age} disabled style={{marginTop: "10px"}}/>              
-            </Box>
+          
+          <TextField id="standard-basic" label="Date de naissance" value={currentRow?.row?.Ddn} disabled style={{marginTop: "10px", width:"100%", marginRight: "5px"}}/>
 
+          <Box
+            sx={{
+              display: "flex",
+              marginTop: "5px",
+              justifyContent: "space-between"
+            }}>
+            <TextField id="standard-basic" label="Nom du Père" value={currentRow?.row?.pere} disabled style={{marginTop: "10px", width:"100%", marginRight: "5px"}}/>
+            <TextField id="standard-basic" label="Nom de la mère" value={currentRow?.row?.mere} disabled style={{marginTop: "10px" ,width: "100%", marginLeft: "5px"}}/>              
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              marginTop: "5px",
+              justifyContent: "space-between"
+            }}>
+            <TextField id="standard-basic" label="Adresse" value={currentRow?.row?.adresse} disabled style={{marginTop: "10px", width:"100%", marginRight: "5px"}}/>
+            <TextField id="standard-basic" label="Fokontany" value={currentRow?.row?.Fokotany} disabled style={{marginTop: "10px" ,width: "100%", marginLeft: "5px"}}/>              
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              marginTop: "5px",
+              justifyContent: "space-between"
+            }}>
+            <TextField id="standard-basic" label="Numéro de téléphone" value={currentRow?.row?.Phone} disabled style={{marginTop: "10px", width:"100%", marginRight: "5px"}}/>
+            <TextField id="standard-basic" label="CIN" value={currentRow?.row?.cin} disabled style={{marginTop: "10px" ,width: "100%", marginLeft: "5px"}}/>              
+          </Box>
+          
           </DialogContentText>
         </DialogContent>
         <DialogActions>
