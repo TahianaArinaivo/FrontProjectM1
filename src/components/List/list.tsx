e4805d27fd64adcfadf725d7e2d1738546acf831
RSitraka <Sitrakahers@gmail.com>
2023-06-02 09:17:45 +0200

    Update
    Dernière modification

--- src/components/List/list.tsx
+++ src/components/List/list.tsx
@@ -101,44 +101,33 @@ const columns: GridColDef[] = [
 ];
 
 const rows = [
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 , Fokotany: 'Ambohipo'},
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 , Fokotany: 'Ambohipo'},
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 , Fokotany: 'Zmbohipo'},
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null , Fokotany: 'Ambohipo'},
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 , Fokotany: 'Ambohipo'},
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 , Fokotany: 'Fmbohipo'},
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 , Fokotany: 'Ambohipo'},
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
-  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO kAPOKA va', mere: 'ANARANA Vehivavy',adresse:'JGD 69 BIS', Ddn:'69 Juillet 1969', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 1, lastName: 'Aloy ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO  Francis', mere: 'RAZEFA',adresse:'FBC 84', Ddn:'21 Mars 1957', Sexe:'Homme', Phone: '0345162578', cin: 169069269369,age: 25 , Fokotany: 'Ankadivato'},
+  { id: 2, lastName: 'Patrick ', firstName: 'RAKOTO', pere: 'RAKOTO Benja', mere: 'ANARANA Sipa',adresse:'FIA 94', Ddn:'16 Juillet 1945', Sexe:'Homme', Phone: '0346960969', cin: 169069269329,age: 35 , Fokotany: 'Ambohipo'},
+  { id: 3, lastName: 'Tendry ', firstName: 'RANDREMA', pere: 'RAKOTO Nirina ', mere: 'Vehivavy',adresse:'FEF 24', Ddn:'12 Septembre 1992', Sexe:'Femme', Phone: '0326957451', cin: 169069269369,age: 23 , Fokotany: 'Analakely'},
+  { id: 4, lastName: 'Kevin ', firstName: 'RANDRIA', pere: 'RAKOTO  Feno', mere: 'MAmaliza',adresse:'ZEFD 685', Ddn:'16 Juillet 1945', Sexe:'Femme', Phone: '0326957451', cin: 169069269369,age: 28 , Fokotany: 'Ankadivato'},
+  { id: 5, lastName: 'Benja ', firstName: 'FENO', pere: 'RAKOTO  Poivron', mere: 'Iaz Marina',adresse:'ZEAF 584', Ddn:'16 Juillet 1945', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 21 , Fokotany: 'Anosy'},
+  { id: 6, lastName: 'Nirina ', firstName: 'KOTOBE', pere: 'RAKOTO  Tomate', mere: 'Test Anarana',adresse:'THE 3554', Ddn:'12 Septembre 1992', Sexe:'Homme', Phone: '0326957451', cin: 169069269369,age: 22 , Fokotany: 'Analakely'},
+  { id: 7, lastName: 'Sitraka ', firstName: 'LETSARA', pere: 'RAKOTO  Oigong', mere: 'Nom',adresse:'NTS 32', Ddn:'16 Juillet 1945', Sexe:'Femme', Phone: '0346960969', cin: 169069269369,age: 23 , Fokotany: 'Ankadivato'},
+  { id: 8, lastName: 'Tahiana ', firstName: 'NIRINA', pere: 'RAKOTO  Crepin', mere: 'Prenom',adresse:' G3G2G 52', Ddn:'12 Septembre 1992', Sexe:'Femme', Phone: '0346960969', cin: 169069269369,age: 25 , Fokotany: 'Anosy'},
+  
+  { id: 9, lastName: 'Portal ', firstName: 'RAZEFA', pere: 'RAKOTO  Histoire', mere: 'Qui',adresse:'ZEFG5 54', Ddn:'16 Juillet 1945', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 24 , Fokotany: 'Ankadivato'},
+  { id: 10, lastName: 'Feno ', firstName: 'BERTRAND', pere: 'RAKOTO  Dortoire', mere: 'Est Le',adresse:'GR3 54', Ddn:'32 Decembre 1968', Sexe:'Femme', Phone: '0326957451', cin: 169069269369,age: 21, Fokotany: 'Analakely'},
+  { id: 11, lastName: 'Alika ', firstName: 'MARCHANDISE', pere: 'RAKOTO Antoine ', mere: 'Nomde',adresse:'EZF 2154', Ddn:'32 Decembre 1968', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 25 , Fokotany: 'Ankadivato'},
+  { id: 12, lastName: 'Anarana ', firstName: 'SACRIFICE', pere: 'RAKOTO  Selstin', mere: 'Fille',adresse:'ZEG 54', Ddn:'32 Decembre 1968', Sexe:'Femme', Phone: '0346960969', cin: 169069269369,age: 27 , Fokotany: 'Analakely'},
+  { id: 13, lastName: 'Iza ', firstName: 'MENTION', pere: 'RAKOTO  ', mere: 'Idiot',adresse:'E2F23 547', Ddn:'21 Decmbre 1985', Sexe:'Homme', Phone: '0326957451', cin: 169069269369,age: 28 , Fokotany: 'Anosy'},
+  { id: 14, lastName: 'Koto ', firstName: 'MAMANA', pere: 'RAKOTO  ', mere: 'Nirina',adresse:'23F2 57', Ddn:'13 Mai 1975', Sexe:'Femme', Phone: '031587548', cin: 169069269369,age: 22 , Fokotany: 'Ankadivato'},
+  { id: 15, lastName: 'Benjamina ', firstName: 'RAVAO', pere: 'RAKOTO  ', mere: 'Zefaniaina',adresse:'AZ 989', Ddn:'01 Avril 1966', Sexe:'Femme', Phone: '0349575268', cin: 169069269369,age: 23 , Fokotany: 'Analakely'},
+  { id: 17, lastName: 'Bema ', firstName: 'RAMANANDRAIBE', pere: 'RAKOTO  Viande', mere: 'Anoinette',adresse:'AZ 54', Ddn:'16 Juillet 1945', Sexe:'Femme', Phone: '0326957451', cin: 169069269369,age: 27 , Fokotany: 'Ankadivato'},
+  { id: 18, lastName: 'Robert ', firstName: 'RAKOTONAVA', pere: 'RAKOTO  Maillot', mere: 'Cotelette',adresse:'GEA 38', Ddn:'12 Septembre 1992', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 24 , Fokotany: 'Analakely'},
+  { id: 19, lastName: 'Julien ', firstName: 'FITIAVANA', pere: 'RAKOTO  Voiture', mere: 'Idiote',adresse:'FRGA 54', Ddn:'16 Juillet 1945', Sexe:'Homme', Phone: '0326957451', cin: 169069269369,age: 25 , Fokotany: 'Ankadivato'},
+  { id: 20, lastName: 'Zefa ', firstName: 'TEST', pere: 'RAKOTO  Fille', mere: '',adresse:'Andreas', Ddn:'NYE 65', Sexe:'Femme', Phone: '0326957451', cin: 169069269369,age: 25 , Fokotany: 'Anosy'},
+  { id: 21, lastName: 'Portal ', firstName: 'RAZEFA', pere: 'RAKOTO  Histoire', mere: 'Qui',adresse:'ZEFG5 54', Ddn:'16 Juillet 1945', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 24 , Fokotany: 'Ankadivato'},
+  { id: 22, lastName: 'Feno ', firstName: 'BERTRAND', pere: 'RAKOTO  Dortoire', mere: 'Est Le',adresse:'GR3 54', Ddn:'32 Decembre 1968', Sexe:'Femme', Phone: '0326957451', cin: 169069269369,age: 21, Fokotany: 'Analakely'},
+  { id: 23, lastName: 'Alika ', firstName: 'MARCHANDISE', pere: 'RAKOTO Antoine ', mere: 'Nomde',adresse:'EZF 2154', Ddn:'32 Decembre 1968', Sexe:'Homme', Phone: '0346960969', cin: 169069269369,age: 25 , Fokotany: 'Ankadivato'},
+  { id: 24, lastName: 'Anarana ', firstName: 'SACRIFICE', pere: 'RAKOTO  Selstin', mere: 'Fille',adresse:'ZEG 54', Ddn:'32 Decembre 1968', Sexe:'Femme', Phone: '0326957451', cin: 169069269369,age: 27 , Fokotany: 'Analakely'},
+  { id: 25, lastName: 'Iza ', firstName: 'MENTION', pere: 'RAKOTO  ', mere: 'Idiot',adresse:'E2F23 547', Ddn:'21 Decmbre 1985', Sexe:'Homme', Phone: '0326957451', cin: 169069269369,age: 28 , Fokotany: 'Anosy'},
+  { id: 26, lastName: 'Koto ', firstName: 'MAMANA', pere: 'RAKOTO  ', mere: 'Nirina',adresse:'23F2 57', Ddn:'13 Mai 1975', Sexe:'Femme', Phone: '031587548', cin: 169069269369,age: 22 , Fokotany: 'Ankadivato'},
+  { id: 27, lastName: 'Benjamina ', firstName: 'RAVAO', pere: 'RAKOTO  ', mere: 'Zefaniaina',adresse:'AZ 989', Ddn:'01 Avril 1966', Sexe:'Femme', Phone: '0349575268', cin: 169069269369,age: 23 , Fokotany: 'Analakely'},
 ];
 
 export default function list() {
@@ -158,14 +147,14 @@ export default function list() {
 
   return (
     <div>
-    <Box sx={{ height: 470, width: '120%' }}>
+    <Box sx={{ height: 500, width: '120%' }}>
       <DataGrid
         rows={rows}
         columns={columns}
         initialState={{
           pagination: {
             paginationModel: {
-              pageSize: 20,
+              pageSize: 15,
             },
           },
         }}
@@ -175,7 +164,7 @@ export default function list() {
         pageSizeOptions={[5]}
         disableRowSelectionOnClick
         onRowClick={handleClickOpen}
-      />
+      />  
     </Box>
     <Dialog
         open={open}
@@ -195,13 +184,12 @@ export default function list() {
               display: "flex",
               justifyContent: ""
             }}>
-          <Card sx={{ maxWidth: 400 }}>
+          <Card sx={{ minWidth: "280px", backgroundColor: "lightgray" , color: "white" }}>
             <CardActionArea>
               <CardMedia
                 component="img"
-                width="500"
-                height="200"
-                image="/public/cin.jpg"
+                height="200px"
+                image="/public/id.jpg"
                 alt="green iguana"
               />
               <CardContent>
@@ -216,11 +204,12 @@ export default function list() {
               sx={{
                 display: "flex",
                 flexDirection: "column",
-                marginLeft: "50px",
+                marginLeft: "20px",
+                marginRight: "auto",
                 marginTop: "5px",
                 justifyContent: "space-between"
               }}>
-                <TextField id="standard-basic" label="Nom" value={currentRow?.row?.firstName} disabled />
+                <TextField id="standard-basic" label="Nom" value={currentRow?.row?.firstName} disabled style={{width: "250px"}}/>
                 <TextField id="standard-basic" label="Prénom" value={currentRow?.row?.lastName} disabled />
                 <TextField id="standard-basic" label="Age" value={currentRow?.row?.age} disabled />       
                 <TextField id="standard-basic" label="Sexe" value={currentRow?.row?.Sexe} disabled />       
